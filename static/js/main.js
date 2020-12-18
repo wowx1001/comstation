3b1700b1bd8decbd8fa1d90f211c702399925312
wowx1001 <56053700+wowx1001@users.noreply.github.com>
2020-12-18 16:48:49 +0900

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -24,12 +24,10 @@ var infowindow = new kakao.maps.InfoWindow({
     removable : true
 });
 
-var index_name = ['type','field_strength','bigo'];
-
 // 마커를 생성하고 지도 위에 마커를 표시하는 함수
 function addMarker(position, idx) {
-    var imageSrc = "https://github.com/wowx1001/comstation/blob/master/static/img/"+(idx%3==0?"1":(idx%3==1?"2":"3"))+".png?raw=true", // 마커 이미지 url, 스프라이트 이미지를 씁니다
-        imageSize = new kakao.maps.Size(17, 17),  // 마커 이미지의 크기
+    var imageSrc = "https://github.com/wowx1001/comstation/blob/master/static/img/"+(idx.substr(0,3)=='송신소'?"1":"3")+".png?raw=true", // 마커 이미지 url, 스프라이트 이미지를 씁니다
+        imageSize = new kakao.maps.Size(15, 15),  // 마커 이미지의 크기
         markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize),
         marker = new kakao.maps.Marker({
             position: position, // 마커의 위치
@@ -43,10 +41,9 @@ function addMarker(position, idx) {
 }
 
 // 조회된 데이터의 인포윈도우 창 추가
-function displayInfowindow(marker,loc,type,field_strength,bigo) {
-    var content = '<div style="font-size:11px; padding:15px;"><div>'+(type?(' 유형 : '+type):"")+'</div>'+
-    '<div>'+(loc?(' 위치 : '+loc):"")+'</div>'+
-    '<div>'+(field_strength?(' 전계강도 : '+field_strength):"")+'</div>'+
+function displayInfowindow(marker,type, field_strength, freq, bigo) {
+    var content = '<div style="font-size:11px; padding:15px;"><div>'+(type?(' 구분 : '+type)+'('+freq+')':"")+'</div>'+
+    '<div>'+(field_strength?(' 전계강도 : '+field_strength+' dB㎶/m'):"")+'</div>'+
     '<div>'+(bigo?(' 비고 : '+bigo):"")+'</div></div>';
     infowindow.setContent(content);
     infowindow.open(map, marker);
@@ -64,35 +61,21 @@ function draw_marker(data){
     // 지도에 표시되고 있는 마커를 제거합니다
     removeMarker(markers);
     
-    for ( var i=0; i<Object.keys(data).length; i++ ) {
-        for (var j=0;j<(Object.keys(data[i]['lat']).length);j++){
-            // 마커를 생성하고 지도에 표시합니다
-            var placePosition = new kakao.maps.LatLng(parseFloat(data[i]['lat'][j]), parseFloat(data[i]['lng'][j])),
-            marker = addMarker(placePosition, i);
-            
-            err_array = [];
-            
-            for (a in index_name){
-                try{
-                    err_array.push(data[i][index_name[a]][j]);               
-                }catch(e){
-                    err_array.push("");  
-                }
-            }
+    for ( var i=0; i<Object.keys(data['번호']).length; i++ ) {
+        // 마커를 생성하고 지도에 표시합니다
+        var placePosition = new kakao.maps.LatLng(parseFloat(data['위도'][i]), parseFloat(data['경도'][i])),
+        marker = addMarker(placePosition, data['구분'][i]);
+        type = data['구분'][i];
+        f_s = data['전계강도(dB㎶/m)'][i];
+        freq = data['주파수'][i];
+        bg = data['비고'][i];
+        (function(marker, type, f_s, freq,bg) {
+            kakao.maps.event.addListener(marker, 'click', function() {
+                infowindow.close();
+                displayInfowindow(marker,type, f_s, freq,bg);
+            });
 
-            loc = data[i]['location'][j];
-            type = err_array[0];
-            f_s = err_array[1];
-            bg = err_array[2];
-
-            (function(marker, loc, type, f_s, bg) {
-                kakao.maps.event.addListener(marker, 'click', function() {
-                    infowindow.close();
-                    displayInfowindow(marker,loc, type, f_s, bg);
-                });
-    
-            })(marker, loc, type, f_s, bg);
-        }
+        })(marker, type, f_s, freq, bg);
     }
 }
   
