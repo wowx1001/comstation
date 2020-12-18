1fdc972df5037dc20f03aac9105e9d4f875b8f66
wowx1001 <56053700+wowx1001@users.noreply.github.com>
2020-12-18 16:50:16 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,7 @@ import pandas as pd
 import flask_jsonpify
 import json
 
-all_com = json.loads(pd.read_csv('data/all_com.csv', encoding='cp949').to_json(orient='columns', force_ascii=False))
-all_relay = json.loads(pd.read_csv('data/all_relay.csv', encoding='cp949').to_json(orient='columns', force_ascii=False))
-jb_com = json.loads(pd.read_csv('data/jb_com.csv', encoding='cp949').to_json(orient='columns', force_ascii=False))
-jb_relay = json.loads(pd.read_csv('data/jb_relay.csv', encoding='cp949').to_json(orient='columns', force_ascii=False))
-jj_com = json.loads(pd.read_csv('data/jj_com.csv', encoding='cp949').to_json(orient='columns', force_ascii=False))
-jj_relay = json.loads(pd.read_csv('data/jj_relay.csv', encoding='cp949').to_json(orient='columns', force_ascii=False))
+all_com = json.loads(pd.read_csv('data/all_com.csv', encoding='utf8').to_json(orient='columns', force_ascii=False))
 app = Flask(__name__)
 
 @app.route('/')
@@ -17,7 +12,7 @@ def home():
 
 @app.route('/req',methods=['POST'])
 def load_data():
-    return flask_jsonpify.jsonify({0:all_com,1:jj_com,2:jb_com,3:all_relay,4:jj_relay,5:jb_relay})
+    return flask_jsonpify.jsonify(all_com)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0",debug=True)
