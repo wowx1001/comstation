4f3511728313e6e015a2578059544a31c223a0de
wowx1001 <56053700+wowx1001@users.noreply.github.com>
2020-12-22 17:51:10 +0900

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -1,14 +1,3 @@
-$.ajax({
-        type: 'POST',
-        url: '/req',
-        contentType: "application/json",
-        success: function(data){
-            draw_marker(data);
-        },
-        error: function(){
-            alert('ajax 통신 실패');
-        }
-})
 var markers = [];
 var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
     mapOption = { 
@@ -26,8 +15,23 @@ var infowindow = new kakao.maps.InfoWindow({
 
 // 마커를 생성하고 지도 위에 마커를 표시하는 함수
 function addMarker(position, idx) {
-    var imageSrc = "https://github.com/wowx1001/comstation/blob/master/static/img/"+(idx.substr(0,3)=='송신소'?"1":"3")+".png?raw=true", // 마커 이미지 url, 스프라이트 이미지를 씁니다
+    var imageSrc = "https://github.com/wowx1001/comstation/blob/master/static/img/1.png?raw=true", // 마커 이미지 url, 스프라이트 이미지를 씁니다
         imageSize = new kakao.maps.Size(15, 15),  // 마커 이미지의 크기
+        markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize),
+        marker = new kakao.maps.Marker({
+            position: position, // 마커의 위치
+            image: markerImage 
+        });
+
+    marker.setMap(map); // 지도 위에 마커를 표출합니다
+    markers.push(marker);  // 배열에 생성된 마커를 추가합니다
+
+    return marker;
+}
+
+function tw_addMarker(position) {
+    var imageSrc = "https://github.com/wowx1001/comstation/blob/master/static/img/Transmission tower.png?raw=true", // 마커 이미지 url, 스프라이트 이미지를 씁니다
+        imageSize = new kakao.maps.Size(30, 30),  // 마커 이미지의 크기
         markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize),
         marker = new kakao.maps.Marker({
             position: position, // 마커의 위치
@@ -42,7 +46,8 @@ function addMarker(position, idx) {
 
 // 조회된 데이터의 인포윈도우 창 추가
 function displayInfowindow(marker,type, field_strength, freq, bigo) {
-    var content = '<div style="font-size:11px; padding:15px;"><div>'+(type?(' 구분 : '+type)+'('+freq+')':"")+'</div>'+
+    var content = '<div style="font-size:11px; padding:15px;"><div>'+(type?(' 구분 : '+type):"")+'</div>'
+    +'<div>'+(freq?(" 주파수 : "+freq):"")+'</div>'+
     '<div>'+(field_strength?(' 전계강도 : '+field_strength+' dB㎶/m'):"")+'</div>'+
     '<div>'+(bigo?(' 비고 : '+bigo):"")+'</div></div>';
     infowindow.setContent(content);
@@ -79,3 +84,41 @@ function draw_marker(data){
     }
 }
   
+function all_region_marker(data){
+    
+    for ( var i=0; i<Object.keys(data['구분']).length; i++ ) {
+        // 마커를 생성하고 지도에 표시합니다
+        var placePosition = new kakao.maps.LatLng(parseFloat(data['위도'][i]), parseFloat(data['경도'][i])),
+        marker = tw_addMarker(placePosition);
+        type = data['구분'][i];
+        (function(marker, type) {
+            kakao.maps.event.addListener(marker, 'click', function() {
+                infowindow.close();
+                displayInfowindow(marker,type);
+            });
+
+        })(marker, type);
+    }
+}
+$.ajax({
+    type: 'POST',
+    url: '/req',
+    contentType: "application/json",
+    success: function(data){
+        draw_marker(data);
+    },
+    error: function(){
+        alert('ajax 통신 실패');
+    }
+})
+$.ajax({
+type: 'POST',
+url: '/tw',
+contentType: "application/json",
+success: function(data){
+    all_region_marker(data);
+},
+error: function(){
+    alert('ajax 통신 실패');
+}
+})
